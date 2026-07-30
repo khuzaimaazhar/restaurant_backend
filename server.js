cfc8d155f28845d9e5afa69c029612222890a405
khuzaimaazhar <khuzaimaazhar6@gmail.com>
2026-07-30 17:25:03 +0500

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ mongoose.connect(process.env.MONGODB_URI)
     .catch(err => console.error(' MongoDB connection error:', err.message));
 
 // Start server
-const PORT = process.env.PORT || 5001;
+const PORT = 5001;
 app.listen(PORT, () => {
     console.log(` Server running on http://localhost:${PORT}`);
 });

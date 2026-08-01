861074f8d7ecb528a5ceb64ff01ee749892604f8
khuzaimaazhar <khuzaimaazhar6@gmail.com>
2026-08-01 23:44:47 +0500

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,12 @@ const app = express();
 console.log('MONGODB_URI from env:', process.env.MONGODB_URI);
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+app.options('*', cors());
 app.use(express.json());
 
 // Import routes
